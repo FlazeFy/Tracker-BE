4db124621bf03d4f69caaa7952673b5aecfd84a9
Leonardho R Sitanggang <79736302+FlazeFy@users.noreply.github.com>
2023-10-18 23:42:45 +0700

    init codebase

---
+++ tracker/routes/routes_v1.go
@@ -0,0 +1,21 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+func InitV1() *echo.Echo {
+	e := echo.New()
+
+	e.GET("api/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Welcome to Pet-Store")
+	})
+
+	// =============== Public routes ===============
+
+	// =============== Private routes (Admin) ===============
+
+	return e
+}
